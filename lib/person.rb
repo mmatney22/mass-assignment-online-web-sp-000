220e7fcd924c01a59e4d1359a0064f933698afb6
Morgan Matney <morgan.matney89@gmail.com>
2020-09-12 14:36:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,10 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-  :weight, :handed, :complexion, :t_shirt_size,
-  :wrist_size, :glove_size, :pant_length, :pant_widthend
+    :weight, :handed, :complexion, :t_shirt_size,
+    :wrist_size, :glove_size, :pant_length, :pant_widthend
+
+  def initialize(attributes)
+
+  end
 
 end
